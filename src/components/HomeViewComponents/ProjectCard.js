9ce347a329374db1995a4a7c1d7395b8e07b8a6c
Salman <shash222@yahoo.com>
2020-07-18 03:36:50 -0400

    Changed HomeView Project Cards

--- src/components/HomeViewComponents/ProjectCard.js
+++ src/components/HomeViewComponents/ProjectCard.js
@@ -7,15 +7,19 @@ export default class ProjectCard extends Component {
             (!window.matchMedia('(max-width: 1300px)').matches)
                 // Desktop Version
                 ? <div className="projectCardWrapper cardWrapper" data-aos="flip-right" data-aos-duration="1000" data-aos-delay={this.props.delay * 150}>
-                    <div className="projectCard card">
+                    <div className="projectCard">
+                        <p className="projectName">{this.props.projectDetail.name}</p>
+                        <p className="projectDescription">{this.props.projectDetail.description}</p>
+
+                    </div>
+                    {/* <div className="projectCard card">
                         <div className="front">
                             <p className="projectName">{this.props.projectDetail.name}</p>
                         </div>
                         <div className="back">
                             <p className="projectDescription">{this.props.projectDetail.description}</p>
-                            {/* <a href={this.props.projectDetail.href} className="projectLink">More</a> */}
                         </div>
-                    </div>
+                    </div> */}
                 </div>
 
                 // Mobile version
